772ce16bfa0b73015c3fea6ff9e7ecc4d780f76c
arcvelit <louka.f.s@gmail.com>
2024-11-14 00:46:04 -0500

    JSON construction and logging demos

---
+++ src/demos/json_print_file.c
@@ -0,0 +1,43 @@
+//#define __JSON_FREE_DEBUG
+
+#include "../json.h" 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Demo setup
+    JSON courses = json_object_alloc();
+    json_add_key_value(courses, "SOEN363", json_decimal_alloc(88.3));
+    json_add_key_value(courses, "ENGR391", json_decimal_alloc(65.4));
+
+    JSON list_nulls = json_array_alloc();
+    json_push(list_nulls, json_null_alloc());
+    json_push(list_nulls, json_null_alloc());
+    json_push(list_nulls, json_null_alloc());
+
+    JSON list = json_array_alloc();
+    json_push(list, courses);
+    json_push(list, json_decimal_alloc(32.3));
+
+    JSON object = json_array_alloc();
+    json_push(object, json_integer_alloc(12));
+    json_push(object, json_string_alloc("Kingslayer"));
+
+    JSON obj = json_object_alloc();
+    json_add_key_value(obj, "Louka", list);
+    json_add_key_value(obj, "Razvan", object);
+    json_add_key_value(obj, "William", json_boolean_alloc(true));
+    json_add_key_value(obj, "NULLS", list_nulls);
+
+    // Print and free
+    if (!logger_file_init("data.json")) 
+        return EXIT_FAILURE;
+
+    json_log(obj);
+    logger_file_close();
+
+    json_free(obj);
+
+    return EXIT_SUCCESS;
+}
